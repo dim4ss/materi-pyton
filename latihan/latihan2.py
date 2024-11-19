856dc4766bb318056d351c9fdca42e2fc2e81d36
dim4ss <dimasghifaria@gmail.com>
2024-11-19 07:54:45 +0700

    menambah perubahan pada file

---
+++ latihan/latihan2.py
@@ -0,0 +1,11 @@
+# program cetak teks sebanyak 100 kali dengan looping
+# inisiasi variabel dengan perulangan
+x = 1
+
+#perulangan sebanyak 100 kali
+while x <= 100 : 
+    #cetak teks
+    print ("saya anak SMK Bina Kerja")
+    print ("Kelas X-PPL")
+    #tambahkan x satu dengan 1 setiap perulangan
+    x+=10
